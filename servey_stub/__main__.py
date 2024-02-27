d854a9976021588ee9884482e71cb63cae3340dc
Tim O'Farrell <tofarr@Tims-MacBook-Pro-2.local>
2024-02-27 16:08:45 -0700

    Fix arguments

--- servey_stub/__main__.py
+++ servey_stub/__main__.py
@@ -8,7 +8,7 @@ from servey.trigger.web_trigger import WebTrigger
 def main():
     parser = argparse.ArgumentParser(description="Servey")
     parser.add_argument("--mode", required=True)
-    parser.add_argument("--outdir", default="servey_stub")
+    parser.add_argument("--output-dir", default="servey_stub")
     parser.add_argument("--only", default="")
     parser.add_argument("--exclude", default="")
     args, _ = parser.parse_known_args()
@@ -30,7 +30,7 @@ def main():
     elif args.exclude:
         exclude = [e.strip() for e in args.exclude.split(",")]
         actions = [action for action in actions if action.name not in exclude]
-    service = Service(actions=actions, output_dir=Path(args.dir))
+    service = Service(actions=actions, output_dir=Path(args.output_dir))
     service.generate_service_code()
 
 
