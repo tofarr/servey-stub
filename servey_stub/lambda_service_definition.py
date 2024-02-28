e6b70f17a790f72ad61267b6ca9d412954df71c9
Tim O'Farrell <tofarr@Tims-MacBook-Pro-2.local>
2024-02-27 19:24:25 -0700

    Blackened

--- servey_stub/lambda_service_definition.py
+++ servey_stub/lambda_service_definition.py
@@ -28,7 +28,9 @@ class LambdaServiceDefinition(ServiceDefinitionABC):
         == 1
     )
     router_name: str = "servey_router"
-    install_requires: List[str] = field(default_factory=lambda: ["boto3", "marshy", "schemey"])
+    install_requires: List[str] = field(
+        default_factory=lambda: ["boto3", "marshy", "schemey"]
+    )
 
     def generate_scaffold_pys(self, package_dir: Path, models_dir: Path):
         super().generate_scaffold_pys(package_dir, models_dir)
